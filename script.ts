217f1d21b4e4281f680fac3e82f4c56a4e913fab
Arpit Bharti <arpitbharti73@gmail.com>
2020-03-31 16:56:50 +0530

    Script for test as yaml file

---
+++ script.ts
@@ -0,0 +1,49 @@
+// import * as YAML from 'yaml';
+import * as YAML from 'js-yaml';
+import * as fs from 'fs';
+
+import { Scenario } from './types';
+
+export const StarWars: Scenario = {
+  Name: 'Star Wars Query Tests',
+  Tests: [
+    {
+      Description: 'Correctly identifies R2-D2 as the hero of the Star Wars Saga',
+      Mock: {
+        Query: `
+        query HeroNameQuery {
+          hero {
+            name
+          }
+        }
+      `
+      },
+      Action: { execute: true },
+      Assertion: {
+        data: {
+          data: {
+            hero: {
+              name: 'R2-D2'
+            }
+          }
+        }
+      }
+    }
+  ]
+};
+
+const obj = {
+  Description: 'Correctly identifies R2-D2 as the hero of the Star Wars Saga',
+  Mock: {
+    Query: `
+    query HeroNameQuery {
+          hero {
+            name
+      }
+  }
+`
+  }
+};
+
+
+fs.writeFileSync('./StarWars.yaml', YAML.safeDump(StarWars));
